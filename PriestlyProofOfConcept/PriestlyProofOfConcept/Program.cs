62d8ca7e8cb4c4cba9507a0772ffa52062ae2af8
Alex <120467993+AlexSchmiediSchmied@users.noreply.github.com>
2024-04-04 11:51:14 +0200

    Add files via upload

---
+++ PriestlyProofOfConcept/PriestlyProofOfConcept/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriestlyProofOfConcept
+{
+    internal class Program
+    {
+        //Project start: 04.04.24
+        //By: A.Y. Schmied :)
+        /*TODO:
+         - Add character selection []
+         - Add intro []
+         - Add stat bars []
+         - Add fight system []
+         - Add enemy 1 -> Demon Lvl. 1 []
+         - Add enemy 2 -> Heretic Lvl. 1 []*/
+        static void Main(string[] args)
+        {
+        }
+    }
+}
