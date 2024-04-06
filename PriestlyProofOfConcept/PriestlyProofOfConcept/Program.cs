f941a41fed1bdb89d77706b779ad68da585e0294
Alex <120467993+AlexSchmiediSchmied@users.noreply.github.com>
2024-04-06 22:30:51 +0200

    Update Program.cs
    Added character selection

--- PriestlyProofOfConcept/PriestlyProofOfConcept/Program.cs
+++ PriestlyProofOfConcept/PriestlyProofOfConcept/Program.cs
@@ -19,6 +19,90 @@ namespace PriestlyProofOfConcept
          - Add enemy 2 -> Heretic Lvl. 1 []*/
         static void Main(string[] args)
         {
+            MchoosePriestOrPriestess();
+
+            MwhichChoiceActive(out string choiceCharacter);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Welcome " + choiceCharacter + "!");
+
+            Console.ReadLine(); 
+        }
+
+        static void MchoosePriestOrPriestess()
+        {
+            string optionPriest = "Priest";
+            string optionPriestess = "Priestess";
+
+                Console.WriteLine("Do you want to play as a Priest or Priestess?");
+                Console.WriteLine("(Use arrow keys to navigate, and press enter)");                         
+        }
+
+        static void MwhichChoiceActive(out string choiceCharacter)
+        {
+            bool choicePriest = true;
+            bool choicePriestess = false;
+
+            string optionPriest = "Priest";
+            string optionPriestess = "Priestess";
+            string auxString = "";
+
+
+            while (true)
+            {/*Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(optionPriest);
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write(" | " + optionPriestess);
+                auxString = optionPriest;*/
+
+                if (Console.ReadKey(true).Key == ConsoleKey.RightArrow)
+                {
+                    choicePriest = false;
+                    choicePriestess = true;
+
+                    Console.Clear();
+
+                    Console.WriteLine("Do you want to play as a Priest or Priestess?");
+                    Console.WriteLine("(Use arrow keys to navigate, and press enter)");
+
+                    Console.Write(optionPriest + " | ");
+                    
+                    Console.ForegroundColor = ConsoleColor.Green;
+
+                    Console.Write(optionPriestess);
+
+                    Console.ForegroundColor = ConsoleColor.Gray;
+
+                    auxString = optionPriestess;
+                }
+
+                if (Console.ReadKey(true).Key == ConsoleKey.LeftArrow)
+                {
+                    choicePriest = true;
+                    choicePriestess = false;
+                    
+                    Console.Clear();
+
+                    Console.WriteLine("Do you want to play as a Priest or Priestess?");
+                    Console.WriteLine("(Use arrow keys to navigate, and press enter)");
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+
+                    Console.Write(optionPriest);
+
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.Write( " | " + optionPriestess);
+
+                    auxString = optionPriest;
+                }
+                if (Console.ReadKey(true).Key == ConsoleKey.Enter)
+                {
+                    break;
+                }
+                //check and output the true choice                
+            }
+            choiceCharacter = auxString;         
         }
     }
 }
